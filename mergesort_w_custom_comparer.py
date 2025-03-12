44af4f54037eb40eee6bc8602503d0765b10fe32
Ivan Ivanov <i.ivanov.bg@abv.bg>
2025-03-12 12:16:08 +0200

    Added custom comparer capability

--- mergesort_w_custom_comparer.py
+++ mergesort_w_custom_comparer.py
@@ -12,11 +12,14 @@ def merge_sort(lst:[],reverse=False,comparer=None)->[]:
         middle_of_list = len(lst)//2
         left_part = lst[:middle_of_list]
         right_part = lst[middle_of_list:]
+        #If no custom comparer has been passed check if normal
+        #or reversed sorting is requested
         if not comparer:
             if not reverse:
                 comparer = lambda x,y : x < y
             else:
                 comparer = lambda x,y : x > y
+        #If custom comparer has been set it will be passed to the merge_left_right function
         #Pass the left part and divide it into smaller parts recursively
         sorted_left_part = merge_sort(left_part,reverse,comparer)
         #Pass the right part and divide it into smaller parts recursively
@@ -27,13 +30,16 @@ def merge_left_right(left_part:[],right_part:[],comparer=None)->[]:
     merge_result = []
     left_pos = right_pos = 0
     while left_pos < len(left_part) and right_pos < len(right_part):
-        #If the left part element's value is smaller than the right part append the
-        #left part element to the list
+        #If the comparer results in True append left element
+        #In the case normal sorting is requested the result will be True if the left element
+        #has smaller value than the right element and it will be appended to the resulting list
+        #In the case reverse sorting is requested the result will be True if the left element
+        #has greater value than the right element and it will be appended to the resulting list
         if comparer(left_part[left_pos],right_part[right_pos]):
             merge_result.append(left_part[left_pos])
             left_pos += 1
         else:
-        #If the right part element's value is greater it will be appended to the list
+        #If the comparer returns False append the right element
             merge_result.append(right_part[right_pos])
             right_pos += 1
     #If one of the parts ( left or right ) has been exhausted for example the left part
@@ -57,4 +63,6 @@ class CustomObject():
 
 unsorted_array_of_objects = [CustomObject(100,50),CustomObject(10,20),CustomObject(30,40)]
 custom_comparer = lambda x,y : x.height < y.height
+print(merge_sort(unsorted_array_of_objects,True,custom_comparer))
+custom_comparer = lambda x,y : x.height > y.height
 print(merge_sort(unsorted_array_of_objects,True,custom_comparer))
